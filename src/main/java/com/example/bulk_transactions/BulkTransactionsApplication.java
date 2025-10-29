2102586251c12f477c2f836b28e005706e799686
chuks-silva <chukwuebuka.ibeh@fairmoney.io>
2025-10-29 16:45:29 +0100

    initial commit and project structure

---
+++ src/main/java/com/example/bulk_transactions/BulkTransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.bulk_transactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulkTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulkTransactionsApplication.class, args);
+	}
+
+}
